93078f7028f97d4649caba9c22c6a43a6eb29403
YUBIN <israelnofeik1@gmail.com>
2024-05-24 02:22:45 -0600

    Página base byte shield
    Recién comienzo de página web de auditoría informática byte shield

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('btnSaludar').addEventListener('click', function() {
+    var mensaje = document.getElementById('mensaje');
+    mensaje.textContent = '¡Hola! Gracias por visitar mi página web.';
+});
